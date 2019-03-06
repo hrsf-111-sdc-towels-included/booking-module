e783fef3c02d4debe7bfaeb3ee89d2ab9318af6e
Sang <Sang@Sang.local>
2019-03-05 16:41:01 -0800

    Update postgreSQL schema to have indexing

--- database/booking-schema-postgre.sql
+++ database/booking-schema-postgre.sql
@@ -15,6 +15,11 @@ CREATE TABLE bookings (
   no_guests integer NOT NULL
 );
 
+CREATE INDEX on bookings (home_id);
+CREATE INDEX on bookings (user_id);
+CREATE INDEX on bookings (check_in);
+CREATE INDEX on bookings (check_out);
+
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
@@ -22,3 +27,8 @@ CREATE TABLE users (
   email VARCHAR(100) NOT NULL,
   login VARCHAR(100) NOT NULL
 );
+
+CREATE INDEX on users (name);
+CREATE INDEX on users (password);
+CREATE INDEX on users (email);
+CREATE INDEX on users (login);
