0ede6b5742d143352a2f1a81a788886b619d84f5
Sang <Sang@Sang.local>
2019-03-06 19:25:30 -0800

    Update all 5 CRUD operations to use postgreSQL and proper db query

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const expressStaticGzip = require('express-static-gzip');
 
-const db = require('../database');
+const db = require('../database/index_pg.js');
 const { cal } = require('./calendarHelper');
 
 const app = express();
@@ -61,29 +61,74 @@ app.post('/api/bookings', (req, res) => {
 });
 
 
-app.route('/api/booking/(:id)?')
+app.route('/api/booking/:id')
   // CREATE
   .post((req, res) => {
-    res.send('create operation success');
+    // addReservation
+    (async function addRes() {
+      try {
+        await db.addReservation({ home_id: req.params.id, body: req.body });
+        res.send('successfully CREATED a reservation');
+      } catch (err) {
+        console.error(err);
+        res.status(500).send();
+      }
+    }());
   })
 
   // READ
   .get((req, res) => {
-    res.send('read operation success');
+    // getAllReservation
+    (async function getRes() {
+      try {
+        const result = await db.getAllReservations({ home_id: req.params.id });
+        res.send(result);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send();
+      }
+    }());
   })
 
   // UPDATE
   .put((req, res) => {
-    res.send('update operation success');
+    // updateReservation
+    (async function updateRes() {
+      try {
+        await db.updateReservation({ home_id: req.params.id, body: req.body });
+        res.send('successfully UPDATED a reservation');
+      } catch (err) {
+        console.error(err);
+        res.status(500).send();
+      }
+    }());
   })
 
   // DELETE
   .delete((req, res) => {
-    res.send('delete operation success');
+    // deleteReservation
+    (async function deleteRes() {
+      try {
+        await db.deleteReservation({ home_id: req.params.id, body: req.body });
+        res.send('delete operation success');
+      } catch (err) {
+        console.error(err);
+        res.status(500).send();
+      }
+    }());
   });
 
-app.get('/api/booking/id/:booking_id(([\\d])+)', (req, res) => {
-  res.send('get booking_id operation success');
+app.get('/api/booking/id/:booking_id(\\d+)', (req, res) => {
+  // getAllReservation
+  (async function getRes() {
+    try {
+      const result = await db.getReservation({ body: { booking_id: req.params.booking_id } });
+      res.send(result);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send();
+    }
+  }());
 });
 
 
